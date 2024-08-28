7d3226fe466fcf16d3ea52682b3d0e0345e71964
Romanuch123 <rmatviyiv@gmail.com>
2024-08-28 19:13:49 +0300

    fix build_and_test.bat

--- ci/build_and_test.bat
+++ ci/build_and_test.bat
@@ -5,21 +5,10 @@ set "RUSTFMT_CI=1"
 rustc -Vv || exit /b 1
 cargo -V || exit /b 1
 
-:: Build and test main crate
+:: Build and test main
 if "%CFG_RELEASE_CHANNEL%"=="nightly" (
     cargo build --locked --all-features || exit /b 1
 ) else (
     cargo build --locked || exit /b 1
 )
-cargo test || exit /b 1
-
-:: Build and test config_proc_macro
-cd config_proc_macro || exit /b 1
-cargo build --locked || exit /b 1
-cargo test || exit /b 1
-
-:: Build and test check_diff
-cd ..
-cd check_diff || exit /b 1
-cargo build --locked || exit /b 1
 cargo test || exit /b 1
